5b922342c6427a838098f2046d2f4f3723b4e16a
tlin7 <tlin7@stuy.edu>
2017-04-23 21:30:13 -0400

    add to helpdesk

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,4 +1,19 @@
-public class HelpDesk
-{
-    private ArrayPriorityQueue<Ticket> = new ArrayPriorityQueue<Ticket>();
-    
+public class HelpDesk {
+    private ArrayPriorityQueue<Ticket> work;
+
+    public HelpDesk(){
+	work = new ArrayPriorityQueue<Ticket>();
+    }
+
+    public String add(int vip, String name, String problem){
+	work.add(new Ticket(vip, name, problem));
+    }
+
+    public Ticket peekMin(){
+	return work.peekMin();
+    }
+
+    public boolean isEmpty(){
+	return work.isEmpty();
+    }
+}
